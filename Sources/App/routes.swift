93c686a5384ce19dfb554d3b6008ef296c804859
Anoimar <vladyslaw@yahoo.com>
2019-06-27 14:42:11 +0200

    new routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,6 +13,28 @@ public func routes(_ router: Router) throws {
     router.post(Animal.self, at: "animal"){(req,animal) in
       return animal.save(on: req)
     }
+    
+    router.get("animal"){ req -> Future<Animal> in
+        let id = try req.query.get(Int.self, at: "id")
+        return Animal.find(id, on: req).unwrap(or: Abort.init(.notFound))
+    }
+    
+    router.delete("animal", Int.parameter) { req -> Future<String> in
+        
+        let id = try req.parameters.next(Int.self)
+        return Animal.find(id, on: req).flatMap { maybeAnimal in
+            
+            guard let animal = maybeAnimal else {
+                throw Abort(.notFound)
+            }
+            
+            return animal.delete(on: req).map {
+                return "Deleted animal: \(animal.name)"
+            }
+        }
+    }
+    
+    
 //    // Example of configuring a controller
 //    let todoController = TodoController()
 //    router.get("todos", use: todoController.index)
